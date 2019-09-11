0c149e5267dc1e50b101aa707c67a983ca69ac4b
OliverSjolind <oliver.sjolind@gmail.com>
2019-09-11 18:06:49 +0300

    finished the javascript

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const arithmeticDisplay = document.getElementById('arithmetics');
 const questionCountDisplay = document.getElementById('questionCounter');
-let questionCounter = 1;
-const numberOfQuestions = 10;
+const mathGame = document.getElementById('math-game')
+const result = document.getElementById('results')
+const resultTexts = document.getElementById('resultTexts')
+const playAgain = document.getElementById('playAgain')
 const questionDisplay = document.getElementById('question')
 let answer1 = document.getElementById('answer1')
 let answer2 = document.getElementById('answer2')
@@ -9,6 +11,8 @@ let answer3 = document.getElementById('answer3')
 let answer4 = document.getElementById('answer4')
 let number1 = Math.floor(Math.random() * 100);
 let number2 = Math.floor(Math.random() * 100);
+let questionCounter = 1;
+const numberOfQuestions = 10;
 let rightCounter = 0;
 let wrongCounter = 0;
 questionDisplay.innerHTML = number1 + ' + ' + number2
@@ -62,9 +66,6 @@ function resetNumbers() {
 }
 
 function guessFunction() {
-    if (questionCounter > numberOfQuestions) {
-
-    }
     if (parseInt(this.innerHTML) === answer) {
         rightCounter += 1
         console.log('Right: ' + rightCounter)
@@ -78,4 +79,22 @@ function guessFunction() {
         resetNumbers();
         console.log(answer);
     }
+    if (questionCounter > numberOfQuestions) {
+        questionCountDisplay.innerHTML = numberOfQuestions + ' / ' + numberOfQuestions;
+        mathGame.style.display = 'none';
+        resultTexts.className = 'row';
+        playAgain.className = 'row playAgain';
+        result.innerText = `You got ${rightCounter} out of ${numberOfQuestions}!`
+    }
+}
+
+playAgain.addEventListener('click', playAgainFunction)
+
+function playAgainFunction() {
+    resultTexts.className = 'hidden';
+    playAgain.className = 'hidden';
+    mathGame.style.display = 'block';
+    rightCounter = 0;
+    questionCounter = 1;
+    resetNumbers();
 }
