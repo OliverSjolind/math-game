ce19e9faded7fe056f9d7488dbb014fc3483c022
OliverSjolind <oliversjolind@gmail.com>
2019-09-12 14:42:46 +0300

    removed duplicated numbers

--- script.js
+++ script.js
@@ -5,51 +5,55 @@ const result = document.getElementById('results')
 const resultTexts = document.getElementById('resultTexts')
 const playAgain = document.getElementById('playAgain')
 const questionDisplay = document.getElementById('question')
-let answer1 = document.getElementById('answer1')
-let answer2 = document.getElementById('answer2')
-let answer3 = document.getElementById('answer3')
-let answer4 = document.getElementById('answer4')
+
 let number1 = Math.floor(Math.random() * 100);
 let number2 = Math.floor(Math.random() * 100);
+
+function addition(number1, number2) {
+    return number1 + number2
+}
+
+let answer = addition(number1, number2);
+
+console.log(answer);
+
 let questionCounter = 1;
 const numberOfQuestions = 10;
 let rightCounter = 0;
 let wrongCounter = 0;
 questionDisplay.innerHTML = number1 + ' + ' + number2
 
-let answers = [
-    answer1,
-    answer2,
-    answer3,
-    answer4
-]
-
-questionCountDisplay.innerHTML = questionCounter + ' / ' + numberOfQuestions
-
-function addition(number1, number2) {
-    return number1 + number2
-}
-
-
-
-let answer = addition(number1, number2);
-console.log(answer);
+let answers = []
 
 let min = answer - 10;
 let max = answer + 10;
 
-answer1.innerHTML = Math.floor(Math.random() * (max - min) + min);
-answer2.innerHTML = Math.floor(Math.random() * (max - min) + min);
-answer3.innerHTML = Math.floor(Math.random() * (max - min) + min);
-answer4.innerHTML = Math.floor(Math.random() * (max - min) + min);
+while (answers.length < 4) {
+    const r = Math.floor(Math.random() * (max - min) + min);
+    if (answers.indexOf(r) === -1 && r != answer) answers.push(r);
+}
 
-answers[Math.floor(Math.random() * 4)].innerHTML = answer
+let answers1 = document.getElementById('answer1')
+let answers2 = document.getElementById('answer2')
+let answers3 = document.getElementById('answer3')
+let answers4 = document.getElementById('answer4')
+
+answers[Math.floor(Math.random() * answers.length)] = answer
+
+answers1.innerHTML = answers[0]
+answers2.innerHTML = answers[1]
+answers3.innerHTML = answers[2]
+answers4.innerHTML = answers[3]
+questionCountDisplay.innerHTML = questionCounter + ' / ' + numberOfQuestions
+
+
 
 answer1.addEventListener('click', guessFunction);
 answer2.addEventListener('click', guessFunction);
 answer3.addEventListener('click', guessFunction);
 answer4.addEventListener('click', guessFunction);
 
+//Reset Functions
 function resetNumbers() {
     number1 = Math.floor(Math.random() * 100);
     number2 = Math.floor(Math.random() * 100);
@@ -59,16 +63,26 @@ function resetNumbers() {
     min = answer - 10;
     max = answer + 10;
 
-    answer1.innerHTML = Math.floor(Math.random() * (max - min) + min);
-    answer2.innerHTML = Math.floor(Math.random() * (max - min) + min);
-    answer3.innerHTML = Math.floor(Math.random() * (max - min) + min);
-    answer4.innerHTML = Math.floor(Math.random() * (max - min) + min);
+    answers = []
 
-    answers[Math.floor(Math.random() * 4)].innerHTML = answer
+    while (answers.length < 4) {
+        const r = Math.floor(Math.random() * (max - min) + min);
+        if (answers.indexOf(r) === -1 && r != answer) answers.push(r);
+    }
+
+    answers[Math.floor(Math.random() * answers.length)] = answer
+
+    answers1.innerHTML = answers[0]
+    answers2.innerHTML = answers[1]
+    answers3.innerHTML = answers[2]
+    answers4.innerHTML = answers[3]
 
     questionDisplay.innerHTML = number1 + ' + ' + number2
 
     questionCountDisplay.innerHTML = questionCounter + ' / ' + numberOfQuestions
+
+    console.log(answer);
+
 }
 
 function guessFunction() {
